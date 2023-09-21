bd2e10aa4ecd54bb3783e9e9e168f5ae5c16e2f3
Masayuki-Tajima <masatajima.work@gmail.com>
2023-09-21 11:58:52 +0900

    トップページ修正

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,10 @@
 @extends('layouts.app')
 
 @section('content')
+<head>
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+</head>
+
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
