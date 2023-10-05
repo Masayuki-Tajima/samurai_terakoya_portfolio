aaec698906d6699a204fd463d8b52be65238b1b8
Masayuki-Tajima <masatajima.work@gmail.com>
2023-10-05 15:48:24 +0900

    ガントチャート実装

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -15,10 +15,14 @@
             <table border="1">
                 <tr>
                     <th>タスク</th>
+                    <th>開始予定日</th>
+                    <th>終了予定日</th>
                 </tr>
                 @foreach($tasks as $task)
                 <tr>
                     <td>{{ $task['name'] }}</td>
+                    <td>{{ $task['scheduled_start_date']}}</td>
+                    <td>{{ $task['scheduled_end_date']}}</td>
                     <td><a href="{{ route('tasks.edit', $task) }}">編集</a></td>
                 </tr>
                 @endforeach
